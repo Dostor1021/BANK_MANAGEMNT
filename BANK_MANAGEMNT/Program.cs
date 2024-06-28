0acd7dff0bbf8762c5ab75930d31e5a414820a60
Dostor1021 <152768388+Dostor1021@users.noreply.github.com>
2024-06-28 20:00:15 +0500

    add

--- BANK_MANAGEMNT/Program.cs
+++ BANK_MANAGEMNT/Program.cs
@@ -1,18 +1,49 @@
 ﻿using System;
 namespace bank_management
 {
-    class login_acount
+   public  class creat_acount_details_list
     {
-
+        public string Name { get;set;}
+        public int ID { get; set; }
+        public int MOBILE_NUM { get; set; }
+        public string ADRESS { get; set; }
+        public string Password  { get; set; }
+        
     }
+    public class acount_create
+    {
+        public List<creat_acount_details_list> Bank = new List<creat_acount_details_list>();
 
+        public void acountcreate()
+        {
 
+            Console.WriteLine("REQUIRED DETAILS");
+            Console.WriteLine("Enter You First And Last Name ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Enter You Id Card Num ");
+            var id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter You Mobile Number");
+            var mbnum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter You Adress ");
+            var adress = Console.ReadLine();
 
+            Bank.Add(new creat_acount_details_list { Name = name ,ID=id,MOBILE_NUM=mbnum,ADRESS=adress});
+            foreach (var i in Bank) 
+            {
+            Console.WriteLine($"{i.Name}\n {i.ADRESS}");
+            
+            }
+        }
+       
 
+   
+    }
 
     class Bank_MAnagement
     {
+       
         
+
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t\t\t\tWELL COME TO BANK OF ISLAMIC");
@@ -24,7 +55,7 @@ namespace bank_management
                 Console.WriteLine("Program stopped.");
                 return;
             }
-            while (input == 1)
+            while (input == 1||input==2)
             {
                 Console.WriteLine("          You have chosen to login or create a new account.");
                 
@@ -37,14 +68,26 @@ namespace bank_management
                     return;
                 }
                 //log acount 
-               else if (input == 1)
+               if (input == 1)
                 {
+
+
                     Console.WriteLine("log in ");
+
+
                 }
                 //creat account 
                else if (input == 2)
                 {
-                    Console.WriteLine("creat acount ");
+                    acount_create acount_Create = new acount_create();
+                    acount_Create.acountcreate();
+                   
+
+               }
+              else
+                {
+                    Console.WriteLine("Program stopped.");
+                    return;
                 }
 
 
